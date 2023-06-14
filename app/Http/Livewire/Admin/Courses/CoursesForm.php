a2a2f32686a4dd3fe90202e6b36dab62390d4450
Ryan Silva <rdsp1@aluno.ifnmg.edu.br>
2023-06-14 10:13:24 -0300

    Criando formulário, lógica e validação de campos para inserção de cursos

--- app/Http/Livewire/Admin/Courses/CoursesForm.php
+++ app/Http/Livewire/Admin/Courses/CoursesForm.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Livewire\Admin\Courses;
 
+use App\Models\Course;
 use Livewire\Component;
 
 class CoursesForm extends Component
 {
+    public Course $course;
+
+    protected $rules = ['course.name' => 'required|string|max:100|min:2|bail'];
+
+    public function mount(){
+        $this->course = new Course;
+    }
+
+    public function save(){
+        $this->validate();
+        $this->course->save();
+        return redirect('/courses');
+    }
+
     public function render()
     {
         return view('livewire.admin.courses.courses-form')->layout('components.admin.layout.layout');
--- resources/views/livewire/admin/courses/courses-form.blade.php
+++ resources/views/livewire/admin/courses/courses-form.blade.php
@@ -1,3 +1,23 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <h1 class="text-2xl text-blue-500 font-medium">Create Courses</h1>
+
+    <form class="mt-4 flex flex-col space-y-4" wire:submit.prevent="save">
+        <label>
+            <span class="text-gray-700">Name</span>
+
+            <input class="mt-1 block w-full rounded-md border-blue-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-500 focus:ring-opacity-50" type="text" wire:model="course.name">
+
+            @error('course.name')
+                <span class="text-red-400">
+                    <small>{{$message}}</small>
+                </span>
+            @enderror
+        </label>
+
+        <div class="self-end">
+            <button type="submit" class="bg-gradient-to-r from-blue-500 to-blue-700 text-blue-50 h-12 px-3 flex items-center rounded-md drop-shadow-lg hover:scale-105 active:scale-95">
+                Save
+            </button>
+        </div>
+    </form>
 </div>
