6d3f352ae72c9d82eb07976eee1efd18692e5d3a
Ryan Silva <rdsp1@aluno.ifnmg.edu.br>
2023-06-13 21:39:03 -0300

    Criando layout principal com menu lateral

---
+++ resources/views/components/admin/layout/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
+    <title>Sistema Academico</title>
+</head>
+<body>
+
+    <x-admin.menu />
+
+    <main class="ml-auto mb-6 w-[85%]">
+        <div class="px-6 pt-6">
+            {{ $slot }}
+        </div>
+    </main>
+</body>
+</html>
---
+++ resources/views/components/admin/menu.blade.php
@@ -0,0 +1,98 @@
+<aside class="fixed top-0 pb-3 px-4 h-screen border-r w-[15%] ml-0">
+    <nav>
+        {{-- Logo --}}
+        <div class="-mx-6 px-6 py-2">
+            <a href="/" class="flex flex-col justify-center items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                    <path d="M11.7 2.805a.75.75 0 01.6 0A60.65 60.65 0 0122.83 8.72a.75.75 0 01-.231 1.337 49.949 49.949 0 00-9.902 3.912l-.003.002-.34.18a.75.75 0 01-.707 0A50.009 50.009 0 007.5 12.174v-.224c0-.131.067-.248.172-.311a54.614 54.614 0 014.653-2.52.75.75 0 00-.65-1.352 56.129 56.129 0 00-4.78 2.589 1.858 1.858 0 00-.859 1.228 49.803 49.803 0 00-4.634-1.527.75.75 0 01-.231-1.337A60.653 60.653 0 0111.7 2.805z" />
+                    <path d="M13.06 15.473a48.45 48.45 0 017.666-3.282c.134 1.414.22 2.843.255 4.285a.75.75 0 01-.46.71 47.878 47.878 0 00-8.105 4.342.75.75 0 01-.832 0 47.877 47.877 0 00-8.104-4.342.75.75 0 01-.461-.71c.035-1.442.121-2.87.255-4.286A48.4 48.4 0 016 13.18v1.27a1.5 1.5 0 00-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.661a6.729 6.729 0 00.551-1.608 1.5 1.5 0 00.14-2.67v-.645a48.549 48.549 0 013.44 1.668 2.25 2.25 0 002.12 0z" />
+                    <path d="M4.462 19.462c.42-.419.753-.89 1-1.394.453.213.902.434 1.347.661a6.743 6.743 0 01-1.286 1.794.75.75 0 11-1.06-1.06z" />
+                  </svg>
+
+                  <span class="ml-1">
+                      SISTEMA ACADEMICO
+                  </span>
+            </a>
+        </div>
+
+        {{-- Link --}}
+        <ul class="space-y-2 mt-6">
+
+            {{-- Cursos --}}
+            <li>
+                <a href="#" class="px-2 py-3 flex items-center space-x-4 group">
+
+                    {{-- Icon --}}
+                    <span class="group-hover:text-blue-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
+                          </svg>
+
+                    </span>
+
+                    {{-- Texto --}}
+                    <span class="group-hover:text-blue-700">
+                        Courses
+                    </span>
+                </a>
+            </li>
+
+            {{-- Disciplinas --}}
+            <li>
+                <a href="#" class="px-2 py-3 flex items-center space-x-4 group">
+
+                    {{-- Icon --}}
+                    <span class="group-hover:text-blue-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+                          </svg>
+
+                    </span>
+
+                    {{-- Texto --}}
+                    <span class="group-hover:text-blue-700">
+                        Disciplines
+                    </span>
+                </a>
+            </li>
+
+            {{-- Professores --}}
+            <li>
+                <a href="#" class="px-2 py-3 flex items-center space-x-4 group">
+
+                    {{-- Icon --}}
+                    <span class="group-hover:text-blue-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                          </svg>
+
+                    </span>
+
+                    {{-- Texto --}}
+                    <span class="group-hover:text-blue-700">
+                        Teachers
+                    </span>
+                </a>
+            </li>
+
+            {{-- Alunos --}}
+            <li>
+                <a href="#" class="px-2 py-3 flex items-center space-x-4 group">
+
+                    {{-- Icon --}}
+                    <span class="group-hover:text-blue-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                          </svg>
+
+                    </span>
+
+                    {{-- Texto --}}
+                    <span class="group-hover:text-blue-700">
+                        Students
+                    </span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</aside>
